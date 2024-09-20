2aae8f92f1cb52168d3c854b8d5caa02a62bada8
dianaRodriguess <dianarodrigues044@gmail.com>
2024-09-20 09:07:27 -0300

    feat: add tela do módulo cadastrar. Resolves #2

--- view/interface.c
+++ view/interface.c
@@ -14,3 +14,16 @@ void telaPrincipal(void){
     printf("|  0. Sair                      |\n");
     printf("|_______________________________|\n\n");
 }
+
+void telaCadastro(){
+    printf(" __________________________________ \n");
+    printf("|.....   MÓDULO DE CADASTRO   .....|\n");
+    printf("|__________________________________|\n");
+    printf("|                                  |\n");
+    printf("|  1. Cadastrar Cliente            |\n");
+    printf("|  2. Cadastrar Treino             |\n");
+    printf("|  3. Cadastrar Funcionário        |\n");
+    printf("|  4. Cadastrar Equipamento        |\n");
+    printf("|  0. Menu principal               |\n");
+    printf("|__________________________________|\n\n");
+}
