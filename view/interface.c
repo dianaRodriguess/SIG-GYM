211c4b7087ba077b43271f3a11686d0bb54c3132
dianaRodriguess <dianarodrigues044@gmail.com>
2024-09-20 08:58:30 -0300

    feat: add tela principal. Resolves #1

---
+++ view/interface.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    
+void telaPrincipal(void){
+    printf(" _______________________________ \n");
+    printf("|.....       SIG-GYM       .....|\n");
+    printf("|_______________________________|\n");
+    printf("|                               |\n");
+    printf("|  1. Módulo Cadastro           |\n");
+    printf("|  2. Módulo Pesquisa           |\n");
+    printf("|  3. Módulo Atualizar          |\n");
+    printf("|  4. Módulo Deletar            |\n");
+    printf("|  5. Relatórios                |\n");
+    printf("|  6. Informações               |\n");
+    printf("|  0. Sair                      |\n");
+    printf("|_______________________________|\n\n");
+}
