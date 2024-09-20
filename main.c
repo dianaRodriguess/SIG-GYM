b6ddef82093a311f04a1e7fb94173d0ea093628e
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-20 15:48:40 -0300

    feat: add parâmetros void nas funções de tela. See issue #8

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "view/interface.c"
+
+int main(void){
+    telaPrincipal();
+    telaCadastro();
+    telaPesquisar();
+    telaAtualizar();
+    telaDeletar();
+    telaRelatorios();
+    telaInfo();
+
+    return 0;
+}
