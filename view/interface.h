77d884993b3f36cc2e15a2adfda35bad8c0a2b29
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-20 15:49:45 -0300

    feat: add assinaturas das funções. Closes #8

---
+++ view/interface.h
@@ -0,0 +1,13 @@
+#if !defined(_INTERFACE_H)
+#define _INTERFACE_H
+
+void telaPrincipal(void);
+void telaCadastro(void);
+void telaPesquisar(void);
+void telaAtualizar(void);
+void telaDeletar(void);
+void telaRelatorios(void);
+void telaInfo(void);
+
+
+#endif // _INTERFACE_H
