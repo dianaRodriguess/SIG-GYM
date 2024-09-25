d534888f9cee70637414b1af68eaf4432fa3b6ca
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-25 09:24:57 -0300

    feat: criando loop principal em main para a intereção entre as telas. Closes #11.

--- main.c
+++ main.c
@@ -2,13 +2,49 @@
 #include "view/interface.c"
 
 int main(void){
-    telaPrincipal();
-    telaCadastro();
-    telaPesquisar();
-    telaAtualizar();
-    telaDeletar();
-    telaRelatorios();
-    telaInfo();
+    int opcao;
+
+    do
+    {
+        opcao = telaPrincipal();
+
+        if(opcao == 0){
+            printf("Saindo...\n");
+            break;
+        }
+
+        int escolha;
+        do
+        {
+            switch (opcao) {
+                case 1:
+                    escolha = telaCadastro();
+                    break;
+                case 2:
+                    escolha = telaPesquisar();
+                    break;
+                case 3:
+                    escolha = telaAtualizar();
+                    break;
+                case 4:
+                    escolha = telaDeletar();
+                    break;
+                case 5:
+                    escolha = telaRelatorios();
+                    break;
+                case 6:
+                    escolha = telaInfo();
+                    break;
+                default:
+                    printf("Opção inválida!\n");
+                    break;
+            }
+
+        } while (escolha != 0);
+        
+
+    } while (1);
+    
 
     return 0;
 }
