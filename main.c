364410c3fb68246b10ec49883d7cbd3c610c424b
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-25 09:59:19 -0300

    bug: concerto do loop infinito em main.c. Closes #14

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "view/interface.c"
 
 int main(void){
     int opcao;
+    int telaAtual = -1;
 
     do
     {
@@ -13,38 +15,44 @@ int main(void){
             break;
         }
 
-        int escolha;
-        do
-        {
-            switch (opcao) {
-                case 1:
-                    escolha = telaCadastro();
-                    break;
-                case 2:
-                    escolha = telaPesquisar();
-                    break;
-                case 3:
-                    escolha = telaAtualizar();
-                    break;
-                case 4:
-                    escolha = telaDeletar();
-                    break;
-                case 5:
-                    escolha = telaRelatorios();
-                    break;
-                case 6:
-                    escolha = telaInfo();
-                    break;
-                default:
-                    printf("Opção inválida!\n");
-                    break;
-            }
+        if(opcao >= 1 && opcao < 7){
 
-        } while (escolha != 0);
-        
+            int escolha;
+
+            do{
+
+                if(opcao != telaAtual){
+                    system("clear");
+                    telaAtual = opcao;
+                }
+
+                switch (opcao) {
+                    case 1:
+                        escolha = telaCadastro();
+                        break;
+                    case 2:
+                        escolha = telaPesquisar();
+                        break;
+                    case 3:
+                        escolha = telaAtualizar();
+                        break;
+                    case 4:
+                        escolha = telaDeletar();
+                        break;
+                    case 5:
+                        escolha = telaRelatorios();
+                        break;
+                    case 6:
+                        escolha = telaInfo();
+                        break;
+                }
+
+            } while (escolha != 0);
+        } else{
+            system("clear");
+        }
 
     } while (1);
-    
 
     return 0;
 }
