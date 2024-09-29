7a74c1eb958d7dced7346a55eda36632611e47ce
dianaRodriguess <dianarodrigues044@gmail.com>
2024-09-29 19:08:56 -0300

    feat: add função limparBuffer()

--- main.c
+++ main.c
@@ -2,18 +2,34 @@
 #include "view/interface.c"
 #include "model/cliente.c"
 
+void limparBuffer(void){
+    int buffer;
+    while((buffer = getchar()) != '\n' && buffer != EOF);
+}
+
 int main(void){
     telaPrincipal();
+    limparBuffer();
     telaCadastro();
+    limparBuffer();
     cadastrarCliente();
+    limparBuffer();
     telaPesquisar();
+    limparBuffer();
     pesquisarCliente();
+    limparBuffer();
     telaAtualizar();
+    limparBuffer();
     atualizarCliente();
+    limparBuffer();
     telaDeletar();
+    limparBuffer();
     deletarCliente();
+    limparBuffer();
     telaRelatorios();
+    limparBuffer();
     telaInfo();
+    limparBuffer();
 
     return 0;
 }
