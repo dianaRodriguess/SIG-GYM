28e075a6d859be1b870c62a6f2fe3f6979989708
dianaRodriguess <dianarodrigues044@gmail.com>
2024-09-29 18:30:31 -0300

    feat: add telas do CRUD de cliente

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include "view/interface.c"
+#include "model/cliente.c"
 
 int main(void){
     telaPrincipal();
     telaCadastro();
+    cadastrarCliente();
     telaPesquisar();
+    pesquisarCliente();
     telaAtualizar();
+    atualizarCliente();
     telaDeletar();
+    deletarCliente();
     telaRelatorios();
     telaInfo();
 
