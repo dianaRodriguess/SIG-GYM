d022407ae2722bdbfaf3251e54ae8bd9097534ed
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-02 19:28:21 -0300

    feat: chamar novas funções de telas e incluir headers no main

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "view/interface.h"
 #include "model/cliente.h"
+#include "model/equipamentos.h"
+#include "model/funcionario.h"
+#include "model/treino.h"
+
 
 void limparBuffer(void){
     int buffer;
@@ -8,27 +13,70 @@ void limparBuffer(void){
 }
 
 int main(void){
+
+    printf("A seguir telas de menu. Clique <ENTER> para prosseguir: \n");
+    limparBuffer();
     telaPrincipal();
     limparBuffer();
     telaCadastro();
     limparBuffer();
-    cadastrarCliente();
-    limparBuffer();
     telaPesquisar();
-    limparBuffer();
-    pesquisarCliente();
     limparBuffer();
     telaAtualizar();
     limparBuffer();
-    atualizarCliente();
-    limparBuffer();
     telaDeletar();
-    limparBuffer();
-    deletarCliente();
     limparBuffer();
     telaRelatorios();
     limparBuffer();
     telaInfo();
+    limparBuffer();
+
+    system("clear");
+    printf("A seguir telas do modulo cliente. Clique <ENTER> para prosseguir: \n");
+    limparBuffer();
+    cadastrarCliente();
+    limparBuffer();
+    pesquisarCliente();
+    limparBuffer();
+    atualizarCliente();
+    limparBuffer();
+    deletarCliente();
+    limparBuffer();
+
+    system("clear");
+    printf("A seguir telas do modulo treino. Clique <ENTER> para prosseguir: \n");
+    limparBuffer();
+    cadastrarTreino();
+    limparBuffer();
+    pesquisarTreino();
+    limparBuffer();
+    atualizarTreino();
+    limparBuffer();
+    deletarTreino();
+    limparBuffer();
+
+    system("clear");
+    printf("A seguir telas do modulo funcionário. Clique <ENTER> para prosseguir: \n");
+    limparBuffer();
+    cadastrarFuncionario();
+    limparBuffer();
+    pesquisarFuncionario();
+    limparBuffer();
+    atualizarFuncionario();
+    limparBuffer();
+    deletarFuncionario();
+    limparBuffer();
+
+    system("clear");
+    printf("A seguir telas do modulo equipamentos. Clique <ENTER> para prosseguir: \n");
+    limparBuffer();
+    cadastrarEquipamento();
+    limparBuffer();
+    pesquisarEquipamento();
+    limparBuffer();
+    atualizarEquipamento();
+    limparBuffer();
+    deletarEquipamento();
     limparBuffer();
 
     return 0;
