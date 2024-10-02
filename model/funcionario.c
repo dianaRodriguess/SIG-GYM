ad012271fad0208208a78eb979126c0d4ff9f545
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-02 18:39:08 -0300

    feat: add telas do módulo funcionario #22

---
+++ model/funcionario.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cadastrarFuncionario(void){
+    system("clear");
+    printf("|_______________________________________________________________|\n");
+    printf("|.....  .  ... . .... CADASTRAR FUNCIONÁRIO .... . ...  .  .....|\n");
+    printf("|_______________________________________________________________|\n");
+    printf("> Nome: \n");
+    printf("> CPF: \n");
+    printf("> Endereço: \n");
+    printf("> Telefone: \n");
+    printf("> Email: \n");
+    printf("> Data de nascimento: \n");
+    printf("> Cargo: \n");
+    printf("\n> Tecle <ENTER> para continuar...\n");
+    printf("|_______________________________________________________________|\n");
+
+}
+
+void pesquisarFuncionario(void){
+    system("clear");
+    printf("|_______________________________________________________________|\n");
+    printf("|.....  .  ... . .... PESQUISAR FUNCIONÁRIO .... . ...  .  .....|\n");
+    printf("|_______________________________________________________________|\n");
+    printf("\n>Digite o CPF do funcionário (apenas números): \n");
+    printf("\n> Tecle <ENTER> para continuar...\n");
+    printf("|_______________________________________________________________|\n");
+}
+
+void atualizarFuncionario(void){
+    system("clear");
+    printf("|_______________________________________________________________|\n");
+    printf("|.....  .  ... . .... ATUALIZAR FUNCIONÁRIO .... . ...  .  .....|\n");
+    printf("|_______________________________________________________________|\n");
+    printf("\n>Digite o CPF do funcionário (apenas números): \n");
+    printf("\n> Tecle <ENTER> para continuar...\n");
+    printf("|_______________________________________________________________|\n");
+}
+
+void deletarFuncionario(void){
+    system("clear");
+    printf("|_______________________________________________________________|\n");
+    printf("|.....  .  ... . ....  DELETAR FUNCIONÁRIO  .... . ...  .  .....|\n");
+    printf("|_______________________________________________________________|\n");
+    printf("\n>Digite o CPF do funcionário (apenas números): \n");
+    printf("\n> Tecle <ENTER> para continuar...\n");
+    printf("|_______________________________________________________________|\n");
+
+}
