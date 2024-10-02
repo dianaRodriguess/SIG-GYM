6c69006e341f80217638bbdd0d75d7b3ab367e36
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-02 18:40:37 -0300

    feat: add telas do módulo treino #23

---
+++ model/treino.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cadastrarTreino(void){
+    system("clear");
+    printf("|__________________________________________________________|\n");
+    printf("|.....  .  ... . .... CADASTRAR TREINO .... . ...  .  .....|\n");
+    printf(" __________________________________________________________|\n");
+    printf("> Nome: \n");
+    printf("> Musculos a serem treinados: \n");
+    printf("> Repetição: \n");
+    printf("> Código de cliente: \n");
+    printf("\n> Tecle <ENTER> para continuar...\n");
+    printf("|__________________________________________________________|\n");
+}
+
+void atualizarTreino(void){
+    system("clear");
+    printf("|__________________________________________________________|\n");
+    printf("|.....  .  ... . .... ATUALIZAR TREINO .... . ...  .  .....|\n");
+    printf(" __________________________________________________________|\n");
+    printf("\n>Digite o Código do treino (apenas números): \n");
+    printf("\n> Tecle <ENTER> para continuar...\n");
+    printf("|__________________________________________________________|\n");
+}
+void pesquisarTreino(void){
+    system("clear");
+    printf("|__________________________________________________________|\n");
+    printf("|.....  .  ... . .... PESQUISAR TREINO .... . ...  .  .....|\n");
+    printf(" __________________________________________________________|\n");
+    printf("\n>Digite o Código do treino (apenas números): \n");
+    printf("\n> Tecle <ENTER> para continuar...\n");
+    printf("|__________________________________________________________|\n");
+}
+void deletarTreino(void){
+    system("clear");
+    printf("|__________________________________________________________|\n");
+    printf("|.....  .  ... . ....  DELETAR TREINO  .... . ...  .  .....|\n");
+    printf(" __________________________________________________________|\n");
+    printf("\n>Digite o Código do treino (apenas números): \n");
+    printf("\n> Tecle <ENTER> para continuar...\n");
+    printf("|__________________________________________________________|\n");
+}
---
+++ model/treino.h
@@ -0,0 +1,9 @@
+#if !defined(_TREINO_H_)
+#define _TREINO_H_
+
+void cadastrarTreino(void);
+void pesquisarTreino(void);
+void atualizarTreino(void);
+void deletarTreino(void);
+
+#endif // _TREINO_H_
