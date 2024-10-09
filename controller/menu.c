106e34f3eb5a94088ce9711881093e99c9123a08
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-09 08:45:34 -0300

    refactor: refatorando a função main.c para um menu

---
+++ controller/menu.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../view/interface.h"
+#include "../model/cliente.h"
+#include "../model/equipamentos.h"
+#include "../model/funcionario.h"
+#include "../model/treino.h"
+#include "entradas.h"
+#include "loops.h"
+
+void menu(void){
+        char opMain;
+    do{
+
+        telaPrincipal();
+        opMain = lerOpcao();
+
+        switch (opMain){
+            case '1':
+                loopCadastrar();
+                break;
+            case '2':
+                loopPesquisar();
+                break;
+
+            case '3':
+                loopAtualizar();
+                break;
+
+            case '4':
+                loopDeletar();
+                break;
+
+            case '5':
+                loopRelatorios();
+                break;
+
+            case '6':
+                telaInfo();
+                pausarTela();
+                break;
+
+            case '0':
+                printf("Obrigado por usar o programa. :)\n");
+                opMain = '0';
+                break;
+
+            default:
+                printf("Opção inválida!\n");
+                pausarTela();
+                break;
+        }
+
+    } while (opMain != '0');
+}
