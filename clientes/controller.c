0e682c608c6510aaf0a01d0626e599a216004378
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-21 20:05:40 -0300

    feat: add lógica para cadastrar cliente

--- clientes/controller.c
+++ clientes/controller.c
@@ -2,9 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include "view.h"
+#include "../view/entradas.h"
+
 
 void cadastrarCliente(void) {
+    char nome[55], cpf[15], endereco[55], telefone[15], email[55], dataNasc[11];
+    char sexo, plano;
+
     menuCadastrarCliente();
+    capturarNome(nome);
+    capturarCPF(cpf);
+    capturarEndereco(endereco);
+    capturarTelefone(telefone);
+    capturarEmail(email);
+    capturarDataNasc(dataNasc);
+    capturarSexo(&sexo);
+    capturarPlano(&plano);
+
+    printf("|_____________________________________________|\n");
+
 }
 
 void pesquisarCliente(void) {
