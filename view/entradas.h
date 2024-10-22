f7c161d8fcd002fd5ad25ecd790804fb8c81b298
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-22 17:23:35 -0300

    docs: add assinatura das funções

--- view/entradas.h
+++ view/entradas.h
@@ -1,19 +1,35 @@
 #ifndef ENTRADAS_H
 #define ENTRADAS_H
 
+// Funções para capturar strings
 void capturarNome(char *nome);
+void capturarFuncao(char *funcao);
+void capturarMarca(char *marca);
 void capturarCPF(char *cpf);
 void capturarEndereco(char *endereco);
 void capturarTelefone(char *telefone);
 void capturarEmail(char *email);
 void capturarDataNasc(char *dataNasc);
+
+// Funções para capturar caracteres
 void capturarSexo(char *sexo);
 void capturarPlano(char *plano);
 
+// Função para capturar código de barras
+void capturarCodBarras(char *codBarras);
+
+// Funções para capturar valores numéricos
+void capturarInt(int *quantidade);
+void capturarFloat(float *preco);
+
+// Função para limpar o buffer de entrada
 void limparBuffer(void);
+
+// Função para pausar a tela
 void pausarTela(void);
 
-char lerOpcao(void);
+// Funções para ler opções do menu
 char lerOpcaoMain(void);
+char lerOpcao(void);
 
-#endif
+#endif // ENTRADAS_H
