47411cd47fbc2faba1866f360b39b62258b77f3a
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-22 17:08:42 -0300

    refactor: movendo funções de exibir as telas de menu

---
+++ equipamentos/controller.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "view.h"
+
+void cadastrarEquipamento(void){
+    menuCadastrarEquipamento();
+    printf("|________________________________________________|\n");
+}
+
+void pesquisarEquipamento(void){
+    menuPesquisarEquipamento();
+    printf("|________________________________________________|\n");
+}
+
+void atualizarEquipamento(void){
+    menuAtualizarEquipamento();
+    printf("|________________________________________________|\n");
+}
+
+void deletarEquipamento(void){
+    menuDeletarEquipamento();
+    printf("|________________________________________________|\n");
+}
---
+++ equipamentos/controller.h
@@ -0,0 +1,9 @@
+#ifndef CONTROLER_H
+#define CONTROLER_H
+
+void cadastrarEquipamento(void);
+void pesquisarEquipamento(void);
+void atualizarEquipamento(void);
+void deletarEquipamento(void);
+
+#endif
