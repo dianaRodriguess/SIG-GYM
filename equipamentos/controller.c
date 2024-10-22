9e6d2919dee7e5e0fb6318d3797b97cdede369b6
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-22 17:27:25 -0300

    feat: add lógica para cadastrar equipamento

--- equipamentos/controller.c
+++ equipamentos/controller.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "view.h"
+#include "../view/entradas.h"
 
 void cadastrarEquipamento(void){
+    char nome[55], funcao[27], marca[27], preco[9];
+    char quantidade[9], codBarras[13];
+
     menuCadastrarEquipamento();
+
+    capturarNome(nome);
+    capturarFuncao(funcao);
+    capturarMarca(marca);
+    capturarPreco(preco);
+    capturarInt(quantidade);
+    capturarCodBarras(codBarras);
+
     printf("|________________________________________________|\n");
 }
 
