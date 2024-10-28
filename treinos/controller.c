19221eb945c33ef307569fcf432f4d89d5ae150b
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-28 01:59:46 -0300

    feat: add declaração das funções de cabeçalho-menu

---
+++ treinos/controller.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "view.h"
+#include "../view/entradas.h"
+
+
+void cadastrarTreino(void) {
+
+
+}
+
+void pesquisarTreino(void) {
+
+}
+
+void atualizarTreino(void) {
+
+}
+
+void deletarTreino(void) {
+
+}
