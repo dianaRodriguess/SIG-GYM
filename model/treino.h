15ac56a3720dca81293b5687e4da2f2570883a53
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-28 01:18:01 -0300

    refactor: funções movidas para nova estrutura MVC treinos/ controller(.c/.h) model(.c/.h) view(.c/.h)

--- model/treino.h
+++
@@ -1,9 +0,0 @@
-#if !defined(_TREINO_H_)
-#define _TREINO_H_
-
-void cadastrarTreino(void);
-void pesquisarTreino(void);
-void atualizarTreino(void);
-void deletarTreino(void);
-
-#endif // _TREINO_H_
