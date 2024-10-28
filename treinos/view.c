7084172be14e06f8b1ede2a4574ea35be7e42f7f
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-28 01:59:08 -0300

    feat: declaração das funções de cabeçalho-menu

---
+++ treinos/view.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "view.h"
+
+
+void menuCadastrarTreino(void){
+
+}
+
+void menuPesquisarTreino(void){
+
+}
+
+void menuAtualizarTreino(void){
+
+}
+
+void menuDeletarTreino(void){
+
+}
