9a8b7939227c342ea27db6ec76266aa5f07ffe76
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-28 01:35:37 -0300

    feat: add assinatura das funções de interface-menu

---
+++ funcionarios/view.h
@@ -0,0 +1,10 @@
+#ifndef FUN_VIEW_H
+#define FUN_VIEW_H
+
+void menuCadastrarFuncionario(void);
+void menuPesquisarFuncionario(void);
+void menuAtualizarFuncionario(void);
+void menuDeletarFuncionario(void);
+
+
+#endif // FUN_VIEW_H
