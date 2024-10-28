fba42f6456befc7ede9b4635f8ed1e6a0e936617
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-28 01:39:40 -0300

    feat: add declaração das funções de controle

---
+++ funcionarios/controller.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "view.h"
+#include "../view/entradas.h"
+
+
+void cadastrarFuncionario(void) {
+
+}
+
+void pesquisarFuncionario(void) {
+
+}
+
+void atualizarFuncionario(void) {
+
+}
+
+void deletarFuncionario(void) {
+
+}
