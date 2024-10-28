8d453bca551c1a8888a1d93ebfef212f3bf5d1f5
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-28 01:36:49 -0300

    feat: add assinatura das funções de controle

---
+++ funcionarios/controller.h
@@ -0,0 +1,10 @@
+#ifndef FUN_CONTROLLER_H
+#define FUN_CONTROLLER_H
+
+void cadastrarFuncionario(void);
+void pesquisarFuncionario(void);
+void atualizarFuncionario(void);
+void deletarFuncionario(void);
+
+
+#endif // FUN_CONTROLLER_H
