64630fb80d154b5f24248037473d56c3e5d0a615
dianaRodriguess <dianarodrigues044@gmail.com>
2024-10-28 01:38:29 -0300

    feat: add declaração das funções de cabeçalho

---
+++ funcionarios/view.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "view.h"
+
+
+void menuCadastrarFuncionario(void){
+
+}
+
+void menuPesquisarFuncionario(void){
+
+}
+
+void menuAtualizarFuncionario(void){
+
+}
+
+void menuDeletarFuncionario(void){
+
+}
