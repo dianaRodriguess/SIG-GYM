556d5430ed2ff98b2b9f732571f02af0279e22cd
PauloDev <paulodouglasmdias@gmail.com>
2024-11-03 16:11:50 -0300

    feat: cria a estrutura (struct) para a função de obter a data atual #50

--- uteis/validacoes.h
+++ uteis/validacoes.h
@@ -1,6 +1,12 @@
 #ifndef VALIDACOES_H
 #define VALIDACOES_H
 
+typedef struct{
+   int dia;
+   int mes;
+   int ano;
+} DataAtual;
+
 int valida_telefone(char*);
 int valida_cpf(char*);
 int verifica_digito(char*, int);
