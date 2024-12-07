0497ea71e14ca46b2d84e0828c5cfd42e209896b
Paulo Douglas <paulodouglasmdias@gmail.com>
2024-12-07 18:37:10 -0300

    feat: cria a função de lê nome do módulo Clientes #80

---
+++ uteis/leitura_dados.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "validacoes.h"
+#include "../view/entradas.h"
+#include "../clientes/model.h"
+
+void leNome(char* nome) {
+    int valido = 0;
+    int tamanho;
+    
+    do {
+        capturarNome(nome);
+        tamanho = strlen(nome);
+
+        if (tamanho == 0) {
+            printf("Nome inválido. Por favor, insira um nome.\n");
+            continue; 
+        }
+
+        if (!validaName(nome, tamanho)) {
+            printf("Nome inválido, tente novamente.\n");
+        } else {
+            valido = 1;  
+        }
+    } while(!valido);
+}
