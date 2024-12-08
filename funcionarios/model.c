fc3c438e2990b25c9365384289fd90e8d995a63b
dianaRodriguess <dianarodrigues044@gmail.com>
2024-12-08 07:30:33 -0300

    feat: add protótipo das funções de funcionário

---
+++ funcionarios/model.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "model.h"
+#include "../view/entradas.h"
+
+// escreve no arquivo de funcionário
+int escreverNoArquivoFun(){
+
+    return TRUE;
+}
+
+// ler o arquivo de funcionário
+int lerArquivoFun(){
+
+    return TRUE;
+}
+
+
+// atualiza o status do funcionário no arquivo
+int atualizarStatusFun(){
+
+    return TRUE;
+}
+
+
+// atualiza os dados do funcionário no arquivo
+int atualizarDadosFun(){
+
+    return TRUE;
+}
