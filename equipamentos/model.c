0874a3d0400d5d4e8c91c75ad851a41b7330cbe5
dianaRodriguess <dianarodrigues044@gmail.com>
2024-12-08 09:33:35 -0300

    feat: add protótipo das funções de funcionário #71

---
+++ equipamentos/model.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "model.h"
+#include "../view/entradas.h"
+
+// escreve no arquivo de equipamentos
+int escreverNoArquivoEqui(){
+
+    return TRUE;
+}
+
+// ler o arquivo de equipamentos
+int lerArquivoEqui(){
+
+    return TRUE;
+}
+
+// atualiza o status do equipamento no arquivo
+int atualizarStatusEqui(){
+
+    return TRUE;
+}
+
+// atualiza os dados do equipamentos no arquivo
+int atualizarDadosEqui(){
+
+    return TRUE;
+}
