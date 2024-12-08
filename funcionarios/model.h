0e4910d8f74dd263109f48b7da9d70b22dbe0533
dianaRodriguess <dianarodrigues044@gmail.com>
2024-12-08 07:29:17 -0300

    feat: add estrutura de funcionário

---
+++ funcionarios/model.h
@@ -0,0 +1,17 @@
+#ifndef _FUN_MODEL_H
+#define _FUN_MODEL_H
+
+#define FALSE 0
+#define TRUE 1
+
+typedef struct {
+    char cpf[15];
+    char nome[55];
+    char cargo[22];
+    char email[55];
+    char telefone[15];
+    char dataNasc[11];
+    char status;
+} Funcionario;
+
+#endif
