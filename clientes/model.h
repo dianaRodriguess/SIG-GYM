1f3f2de0550a872ffbeab97746be0b7c725be057
Paulo Douglas <paulodouglasmdias@gmail.com>
2024-12-10 12:25:11 -0300

    feat: cria enums para Sexo, Plano e Status, simplificando o uso e manutenção do sistema #89

--- clientes/model.h
+++ clientes/model.h
@@ -7,14 +7,31 @@ typedef struct {
     char telefone[12];
     char email[55];
     char dataNasc[11];
-    char sexo;
-    char plano;
-    int status;
+    Sexo sexo;
+    Plano plano;
+    Status status;
 } Cliente;
+
+typedef enum {
+    Masculino = 1,
+    Feminino = 2
+} Sexo;
+
+typedef enum {
+    Básico = 1,
+    Intermediário = 2,
+    Avançado = 3
+} Plano;
+
+typedef enum {
+    Ativo = 1,
+    Inativo = 0
+} Status;
 
 int salvarCliente(Cliente*);
 Cliente* carregarClientes(char*);
 int regravaCliente(Cliente*);
 int excluirCliente(Cliente*, char*);
+void alteraCliente(Cliente*, int);
 
 #endif
