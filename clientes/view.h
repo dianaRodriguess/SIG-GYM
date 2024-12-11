6d21e7e0e2ea812adfe9ebfa0f81d5e9fdf0777e
Paulo Douglas <paulodouglasmdias@gmail.com>
2024-12-11 20:59:13 -0300

    fix: remove funções inativas do arquivo view.h #89

--- clientes/view.h
+++ clientes/view.h
@@ -1,9 +1,6 @@
 #ifndef VIEW_H
 #define VIEW_H
 
-void menuCadastrarCliente(void);
-void menuPesquisarCliente(void);
-void menuAtualizarCliente(void);
 void menuDeletarCliente(void);
 void mostrarEquipamentos(Cliente* cli);
 char menuClientes(void);
