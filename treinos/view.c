b95baf2a9ae6cf18282a71ba1f9575b0fc5c0fa9
André Leandro <speykergamer@gmail.com>
2025-01-02 15:19:04 -0300

    feat: add tela de treino #70

--- treinos/view.c
+++ treinos/view.c
@@ -3,30 +3,47 @@
 #include "view.h"
 
 
-void menuCadastrarTreino(void){
-    system("clear");
-    printf("|____________________________________________|\n");
-    printf("|.....  .  ... CADASTRAR TREINO ...  .  .....|\n");
-    printf("|____________________________________________|\n");
+char menuTreinos(void){
+    limparTela();
+    char opcao;
+    printf("----------------------------------------------\n");
+    printf("|                  Treinos                   |\n");
+    printf("----------------------------------------------\n");
+    printf("|             1 - Cadastrar Treino           |\n");
+    printf("|             2 - Pesquisar Treino           |\n");
+    printf("|             3 - Atualizar Treino           |\n");
+    printf("|             4 - Deletar Treino             |\n");
+    printf("|             0 - Retornar ao Menu Principal |\n");
+    printf("----------------------------------------------\n");
+    printf("> Selecione uma opção: ");
+    scanf(" %c", &opcao);
+    return opcao;
 }
 
-void menuPesquisarTreino(void){
-    system("clear");
-    printf("|____________________________________________|\n");
-    printf("|.....  .  ... PESQUISAR TREINO ...  .  .....|\n");
-    printf("|____________________________________________|\n");
+void menuCadastrarTreino(void){
+    limparTela();
+    printf("----------------------------------------------\n");
+    printf("|             Cadastro de Treino              |\n");
+    printf("----------------------------------------------\n");
+}
+
+void menuExibirTreino(void){
+    limparTela();
+    printf("----------------------------------------------\n");
+    printf("|                Exibir Treino               |\n");
+    printf("----------------------------------------------\n");
 }
 
 void menuAtualizarTreino(void){
-    system("clear");
-    printf("|____________________________________________|\n");
-    printf("|.....  .  ... ATUALIZAR TREINO ...  .  .....|\n");
-    printf("|____________________________________________|\n");
+    limparTela();
+    printf("----------------------------------------------\n");
+    printf("|                Alterar Treino              |\n");
+    printf("----------------------------------------------\n");
 }
 
 void menuDeletarTreino(void){
-    system("clear");
-    printf("|____________________________________________|\n");
-    printf("|......  .  ... DELETAR TREINO ...  .  ......|\n");
-    printf("|____________________________________________|\n");
+    limparTela();
+    printf("----------------------------------------------\n");
+    printf("|                Deletar Treino              |\n");
+    printf("----------------------------------------------\n");
 }
