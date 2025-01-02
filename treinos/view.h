6e23092e0190803dfbff206e75ee777abb215000
André Leandro <speykergamer@gmail.com>
2025-01-02 12:40:34 -0300

    feat: add telas de treinos #96

--- treinos/view.h
+++ treinos/view.h
@@ -1,8 +1,9 @@
 #ifndef TRE_VIEW_H
 #define TRE_VIEW_H
 
+char menuTreinos(void);
 void menuCadastrarTreino(void);
-void menuPesquisarTreino(void);
+void menuExibirTreino(void);
 void menuAtualizarTreino(void);
 void menuDeletarTreino(void);
 
