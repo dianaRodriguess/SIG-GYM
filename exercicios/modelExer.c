676dda6241a020691872e55345c54b2dcd3ff913
André Leandro <speykergamer@gmail.com>
2025-01-22 18:28:57 -0300

    update: update manual

--- exercicios/modelExer.c
+++ exercicios/modelExer.c
@@ -1,1 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "../libs/utils.h"
+#include "modelExer.h"
+#include "../libs/leitura_dados.h"
+#include "viewExer.h"
+
+int salvarExercicio(Exercicio* exercicio){
+    FILE* arquivo = fopen("exercicios.dat", "ab");
+    if(arquivo == NULL){
+        return 0;
+    }
+    if(fwrite(exercicio, sizeof(Exercicio), 1, arquivo) != 1){
+        return -1;
+    }
+    fclose(arquivo);
+    return 1;
+}
+
+Exercicio* carregarExercicio(int id){
+    Exercicio* exercicio = (Exercicio*)malloc(sizeof(Exercicio));
+    FILE* arquivo = fopen("exercicios.dat", "rb");
+    if(arquivo == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return NULL;
+    }
+
+    while(fread(exercicio, sizeof(Exercicio), 1, arquivo)) {
+        if (exercicio->id_exercicio == id){
+            fclose(arquivo);
+            return exercicio;
+        }
+    }
+    fclose(arquivo);
+    free(exercicio);
+    return NULL;
+}
+
+void alteraExercicio(Exercicio* exercicio, int op){
+    char* entrada = NULL;
+
+    switch(op){
+        case 1:
+            limparBuffer();
+            entrada = leNome(); // Chama a função e armazena o resultado em entrada
+            strcpy(exercicio->nome, entrada); // Copia o valor para o campo nome
+            break;
+
+        case 2:
+            limparBuffer();
+            entrada = leFuncao(); // Chama a função e armazena o resultado em entrada
+            strcpy(exercicio->categoria, entrada); // Copia o valor para o campo categoria
+            break;
+
+        default:
+            printf("Opção inválida\n");
+            break;
+    }
+
+    regravaExercicio(exercicio); // ainda irei fazer
+    dadosExercicios(exercicio); // ainda irei fazer
+    if (entrada != NULL){
+        free(entrada);
+    }
+}
+int regravaExercicio(Exercicio* exercicio){
+    FILE *arquivo = fopen("exercicios.dat", "r+b");
+    if(arquivo == NULL){
+        return 0;  
+    }
+
+    Exercicio* novoExercicio = (Exercicio*)malloc(sizeof(Exercicio));
+    if (novoExercicio == NULL) {
+        fclose(arquivo);  
+        return 0;  
+    }
+
+    while (fread(novoExercicio, sizeof(Exercicio), 1 , arquivo)){
+        if (novoExercicio->id_exercicio == exercicio->id_exercicio) {
+            fseek(arquivo, -sizeof(Exercicio), SEEK_CUR);
+            if(fwrite(exercicio, sizeof(Exercicio), 1, arquivo)){
+                fclose(arquivo);
+                free(novoExercicio);
+                return TRUE; 
+            } 
+            fclose(arquivo);
+            free(novoExercicio);
+            return -1;  
+        }
+    }
+
+    fclose(arquivo);
+    free(novoExercicio);
+    return -2;  // Exercício não encontrado
+}
+
+
+int deletarExercicio(Exercicio* exercicio){
+    if(exercicio->status == 1){
+        exercicio->status = 0;
+        regravaExercicio(exercicio); // ainda irei fazer
+        return 1;
+    }else{
+        return -1;
+    }
+
+    return -2;
+}
+
+int checaExercicioID(int id){
+    char op;
+    Exercicio* exercicio = carregarExercicio(id);
+
+    if(exercicio != NULL){
+        if(exercicio->status == 0){
+            op = confirmação("cliente", "você possui um exercicio inativo no nosso sistema. Deseja reativá-la?");
+            switch(op) {
+            case '1':
+                exercicio->status = 1;
+                regravaExercicio(exercicio);
+                free(exercicio);
+                return 1;
+            case '0':
+                free(exercicio);
+                return 0;
+            default:
+                printf("Opção inválida\n");
+                break;
+            }
+        } else if (exercicio->status == 1){
+            return -1;
+        }
+    } else {
+       return 0;
+    }
+
+    return -4;
+}
--- exercicios/viewExer.c
+++ exercicios/viewExer.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "view.h"
+#include "viewExer.h"
+#include "modelExer.h"
+#include "../libs/utils.h"
 
 
-char menuTreinos(void){
+char menuExercicios(void){
     limparTela();
     char opcao;
     printf("----------------------------------------------\n");
-    printf("|                  Treinos                   |\n");
+    printf("|                  Exercícios                |\n");
     printf("----------------------------------------------\n");
-    printf("|             1 - Cadastrar Treino           |\n");
-    printf("|             2 - Pesquisar Treino           |\n");
-    printf("|             3 - Atualizar Treino           |\n");
-    printf("|             4 - Deletar Treino             |\n");
+    printf("|             1 - Cadastrar Exercício        |\n");
+    printf("|             2 - Pesquisar Exercício        |\n");
+    printf("|             3 - Atualizar Exercício        |\n");
+    printf("|             4 - Excluir exercício          |\n");
+    printf("|             5 - Relatórios                 |\n");
     printf("|             0 - Retornar ao Menu Principal |\n");
     printf("----------------------------------------------\n");
     printf("> Selecione uma opção: ");
@@ -20,30 +23,71 @@ char menuTreinos(void){
     return opcao;
 }
 
-void menuCadastrarTreino(void){
+void menuCadastrarExercicio(void){
     limparTela();
     printf("----------------------------------------------\n");
-    printf("|             Cadastro de Treino              |\n");
+    printf("|             Cadastro de Exercicio          |\n");
     printf("----------------------------------------------\n");
 }
 
-void menuExibirTreino(void){
+void menuExibirExercicio(void){
     limparTela();
     printf("----------------------------------------------\n");
-    printf("|                Exibir Treino               |\n");
+    printf("|               Exibir Exercicio             |\n");
     printf("----------------------------------------------\n");
 }
 
-void menuAtualizarTreino(void){
+void menuAtualizarExercicio(void){
     limparTela();
     printf("----------------------------------------------\n");
-    printf("|                Alterar Treino              |\n");
+    printf("|               Alterar Exercicio            |\n");
     printf("----------------------------------------------\n");
 }
 
-void menuDeletarTreino(void){
+void menuDeletarExercicio(void){
     limparTela();
     printf("----------------------------------------------\n");
-    printf("|                Deletar Treino              |\n");
+    printf("|               Deletar Exercicio            |\n");
     printf("----------------------------------------------\n");
+}
+
+void dadosExercicios(Exercicio* exercicio){
+    limparTela();
+    printf("|\t\t\033[1m-> Dados do Exercício\033[m \n");
+    printf("\033[1m|Nome:\033[m %s\n", exercicio->nome);
+    printf("\033[1m|ID:\033[m %d\n", exercicio->id_exercicio);
+    printf("\033[1m|Categoria:\033[m %s\n", exercicio->categoria);
+    printf("|+---------------------------------------------------------------------+-----------------------------------------------------------------------+|\n");
+}
+
+char relatorioExercicio(void){
+    limparTela();
+    char opcao;
+    printf("-------------------------------------------------------\n");
+    printf("|                       Relatório                     |\n");
+    printf("-------------------------------------------------------\n");
+    printf("|              1 - Status dos exercícios              |\n");
+    printf("|              2 - Exercícios por ordem alf.          |\n");
+    printf("|              0 - Retornar ao Menu Principal         |\n");
+    printf("-------------------------------------------------------\n");
+    printf("> Selecione uma opção: ");
+    scanf(" %c", &opcao);
+    return opcao;
+}
+
+
+char statusExercicios(void){
+    limparTela();
+    char opcao;
+    printf("-------------------------------------------------------\n");
+    printf("|                   Relatório - Status                |\n");
+    printf("-------------------------------------------------------\n");
+    printf("|              1 - Exercícios ativos                  |\n");
+    printf("|              2 - Exercícios inativos                |\n");
+    printf("|              3 - Todos os exercícios                |\n");
+    printf("|              0 - Retornar ao Menu Principal         |\n");
+    printf("-------------------------------------------------------\n");
+    printf("> Selecione uma opção: ");
+    scanf(" %c", &opcao);
+    return opcao;
 }
