ea488135fff9811ff5dc27c10729f1a1fe781d94
Paulo Douglas <paulodouglasmdias@gmail.com>
2025-01-22 17:38:19 -0300

    refactor: altera o nome do arquivo para melhor funcionamento #107

---
+++ equipamentos/controllerEqui.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+#include "modelEqui.h"
+
+void menuEquipamentos(void);
+Equipamento* CadastroEquipamento(void);
+void listarEquipamento(void);
+void editarEquipamento(void);
+void excluirEquipamento(void);
+
+#endif // CONTROLLER_H
