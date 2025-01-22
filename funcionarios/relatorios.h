3abaedc93a74a04447e39b335e60e55393de837a
André Leandro <speykergamer@gmail.com>
2025-01-22 08:50:30 -0300

    feat: add funções para armazenar apenas os relatorios

---
+++ funcionarios/relatorios.h
@@ -0,0 +1,14 @@
+#ifndef RELATORIOS_H
+#define RELATORIOS_H
+
+void ativosDadosFuncionarios();
+void inativosDadosFuncionarios();
+void allFuncionarios();
+void dadosFuncionariosGerente();
+void dadosFuncionariosProfessor();
+void dadosFuncionariosASG();
+void dadosFuncionariosAtendente();
+
+
+#endif
+
