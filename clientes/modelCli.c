8f1b310b16b2d87fcd71525efa081589edfff191
Paulo Douglas <paulodouglasmdias@gmail.com>
2025-01-22 17:35:53 -0300

    refactor: altera o nome do arquivo para melhor funcionamento #107

--- clientes/modelCli.c
+++ clientes/modelCli.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "model.h"
-#include "view.h"
+#include "modelCli.h"
+#include "viewCli.h"
 #include "../libs/utils.h"
 #include "../libs/style.h"
 #include "../libs/leitura_dados.h"
