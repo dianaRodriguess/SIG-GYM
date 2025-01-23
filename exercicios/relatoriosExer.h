36815427a5fa9cf866d6ea4844f4f798992afbaa
André Leandro <speykergamer@gmail.com>
2025-01-23 09:01:04 -0300

    fix: ajustando os parametros no cabeçario de model de relatorio de Exercicio

--- exercicios/relatoriosExer.h
+++ exercicios/relatoriosExer.h
@@ -1,7 +1,7 @@
 #ifndef RELATORIOS_H
 #define RELATORIOS_H
 
-void ativosExercicios();
-void inativosExercicios();
-void allExercicios();
+void ativosExercicios(void);
+void inativosExercicios(void);
+void allExercicios(void);
 #endif
